1cf1c8c15570ce1069aabee2061a2d62634eb64c
Rishi Mukherjee <rishirishi20121997@gmail.com>
2025-01-27 05:07:05 +0530

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
+
+export default [
+  index("routes/home.tsx"),
+  route("/share-target", "routes/share-target.tsx"),
+] satisfies RouteConfig;
+
